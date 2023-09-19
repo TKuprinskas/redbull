a57275338d51f4652830084312fc3ba142973e00
Tomas Kuprinskas <t.kuprinskas@gmail.com>
2023-09-19 18:47:49 +0300

    some updates

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -17,111 +17,124 @@ import redbull from '../../src/assets/images/redbull.jpg';
 import { loginAsync } from '../services/API';
 
 function Copyright(props) {
-    return (
-        <Typography variant="body2" color="text.secondary" align="center" {...props}>
-            {'Copyright © '}
-            <Link color="inherit" href="https://redbull.tenisopartneris.lt/">
-                RedBull Lithuania
-            </Link>{' '}
-            {new Date().getFullYear()}
-            {'.'}
-        </Typography>
-    );
+  return (
+    <Typography
+      variant='body2'
+      color='text.secondary'
+      align='center'
+      {...props}>
+      {'Copyright © '}
+      <Link color='inherit' href='https://redbull.tenisopartneris.lt/'>
+        MV Group Distribution LT
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
 }
 
 const theme = createTheme();
 
 export default function SignIn() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-        const data = new FormData(event.currentTarget);
-        await loginAsync(data.get('username'), data.get('password')).then((response) => {
-            if (response.status === 'SUCCESS') {
-                setTimeout(() => {
-                    navigate('/home', { replace: true });
-                }, 1500);
-            } else {
-                return;
-            }
-        });
-    };
-
-    return (
-        <ThemeProvider theme={theme}>
-            <Container component="main" maxWidth="xs">
-                <ToastContainer
-                    position="top-center"
-                    autoClose={1500}
-                    hideProgressBar={false}
-                    newestOnTop={false}
-                    closeOnClick
-                    rtl={false}
-                    pauseOnFocusLoss
-                    draggable
-                    pauseOnHover
-                />
-                <CssBaseline />
-                <Box
-                    sx={{
-                        marginTop: 8,
-                        display: 'flex',
-                        flexDirection: 'column',
-                        alignItems: 'center',
-                    }}
-                >
-                    <Box
-                        component="img"
-                        sx={{
-                            height: 233,
-                            width: 350,
-                            maxHeight: { xs: 150, md: 170 },
-                            maxWidth: { xs: 220, md: 250 },
-                            mb: 2,
-                            borderRadius: 5,
-                        }}
-                        alt="redbull"
-                        src={redbull}
-                    />
-                    <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-                        <LockOutlinedIcon />
-                    </Avatar>
-                    <Typography component="h1" variant="h5">
-                        Prisijungimas
-                    </Typography>
-                    <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-                        <TextField
-                            margin="normal"
-                            required
-                            fullWidth
-                            id="username"
-                            label="Username"
-                            name="username"
-                            autoComplete="username"
-                            autoFocus
-                        />
-                        <TextField
-                            margin="normal"
-                            required
-                            fullWidth
-                            name="password"
-                            label="Password"
-                            type="password"
-                            id="password"
-                            autoComplete="current-password"
-                        />
-                        <FormControlLabel
-                            control={<Checkbox value="remember" color="primary" />}
-                            label="Prisiminti mane"
-                        />
-                        <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
-                            Prisijungti
-                        </Button>
-                    </Box>
-                </Box>
-                <Copyright sx={{ mt: 8, mb: 4 }} />
-            </Container>
-        </ThemeProvider>
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    await loginAsync(data.get('username'), data.get('password')).then(
+      (response) => {
+        if (response.status === 'SUCCESS') {
+          setTimeout(() => {
+            navigate('/home', { replace: true });
+          }, 1500);
+        } else {
+          return;
+        }
+      }
     );
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component='main' maxWidth='xs'>
+        <ToastContainer
+          position='top-center'
+          autoClose={1500}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+        />
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}>
+          <Box
+            component='img'
+            sx={{
+              height: 200,
+              width: 200,
+              maxHeight: { xs: 120, md: 150, xxl: 200 },
+              maxWidth: { xs: 120, md: 150, xxl: 200 },
+              mb: 2,
+              borderRadius: 5,
+            }}
+            alt='redbull'
+            src={redbull}
+          />
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component='h1' variant='h5'>
+            Prisijungimas
+          </Typography>
+          <Box
+            component='form'
+            onSubmit={handleSubmit}
+            noValidate
+            sx={{ mt: 1 }}>
+            <TextField
+              margin='normal'
+              required
+              fullWidth
+              id='username'
+              label='Username'
+              name='username'
+              autoComplete='username'
+              autoFocus
+            />
+            <TextField
+              margin='normal'
+              required
+              fullWidth
+              name='password'
+              label='Password'
+              type='password'
+              id='password'
+              autoComplete='current-password'
+            />
+            <FormControlLabel
+              control={<Checkbox value='remember' color='primary' />}
+              label='Prisiminti mane'
+            />
+            <Button
+              type='submit'
+              fullWidth
+              variant='contained'
+              sx={{ mt: 3, mb: 2 }}>
+              Prisijungti
+            </Button>
+          </Box>
+        </Box>
+        <Copyright sx={{ mt: 8, mb: 4 }} />
+      </Container>
+    </ThemeProvider>
+  );
 }
